8ff0e85f59018f1bcf55d0fc29f90b97378dcd03
FranckyPoop <afrifranck2003@gmail.com>
2022-06-12 18:33:23 +0000

    Il est désormais posible de s'inscrit a la newslater

---
+++ src/front/static/front/js/sweetalert.js
@@ -0,0 +1,8 @@
+htmx.on("showMessage", (event) => {
+    Swal.fire({
+        icon: event.detail.icon,
+        title: event.detail.title,
+        text: event.detail.message,
+    })
+})
+
--- src/front/views.py
+++ src/front/views.py
@@ -1,10 +1,11 @@
-import re
+from re import search
+import json
 from features.models import Newsletter
 from .models import *
 from django.db.utils import IntegrityError
 from django.shortcuts import render, redirect
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 
 class RequestIndex(View):
@@ -25,18 +26,64 @@ class RequestIndex(View):
     def post(self, request):
         regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
         email = request.POST["email_newlater"]
-        
-        
-        if re.search(regex, email):  
+
+        if search(regex, email):  
             try:
                 data = Newsletter.objects.create(email=email)  
                 data.save()
-                messages.add_message(request, messages.SUCCESS, 'Félicitation vous venez de vous incrit a la newslatter')
-                return HttpResponse("")
+                return HttpResponse(
+                    status=204,
+                    headers={
+                        "HX-Trigger": json.dumps({
+                            "showMessage": {
+                                "message": "Félicitation vous venez de vous inscrit a la newslater",
+                                "icon": "success",
+                                "title": "Newslater"
+                            }
+                        })
+                    }
+                )
             except IntegrityError:
-                messages.add_message(request, messages.ERROR, 'Cette addresse email existe déjà dans la newslatter')
+                return HttpResponse(
+                    status=204, 
+                    headers={
+                        "HX-Trigger": json.dumps({
+                            "showMessage": {
+                                "message": "Cette addresse email est déjà inscrite dans la newslatter",
+                                "icon": "info",
+                                "title": "Newslater"
+                            }
+                        })
+                    }
+                )
+        elif not email:
+            return HttpResponse(
+                    status=204, 
+                    headers={
+                        "HX-Trigger": json.dumps({
+                            "showMessage": {
+                                "message": "Veuillez remplir le champ pour vous incrit a la newslater",
+                                "icon": "error",
+                                "title": "Newslater"
+                            }
+                        })
+                    }
+                )
+        return HttpResponse(
+                    status=204, 
+                    headers={
+                        "HX-Trigger": json.dumps({
+                            "showMessage": {
+                                "message": "Veuillez entrer une addresse email valid",
+                                "icon": "error",
+                                "title": "Newslater"
+                            }
+                        })
+                    }
+                )
+                    
         
-        return self.get(request)
+        
 
 def front_error_404(request):
     return render(request, "front/pages/404.html")
